bbaabc7c0c86d7aa09373834bba45ec1099b7254
Avamaco <Maco113002@gmail.com>
2023-04-01 20:20:54 +0200

    Fixed phial of propulsion explosion effect

--- src/main/java/net/avamaco/alchemicalutilities/item/custom/phial/PhialOfPropulsionItem.java
+++ src/main/java/net/avamaco/alchemicalutilities/item/custom/phial/PhialOfPropulsionItem.java
@@ -6,11 +6,15 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.block.piston.PistonBaseBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import java.lang.reflect.Field;
 
 public class PhialOfPropulsionItem extends PotionPhialItem {
     public PhialOfPropulsionItem(Properties pProperties) {
@@ -20,7 +24,7 @@ public class PhialOfPropulsionItem extends PotionPhialItem {
 
     @Override
     public void UseOnEntity(LivingEntity entity, Entity user) {
-        entity.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 20, 5), user);
+        entity.push(0.0D, 2.0D, 0.0D);
     }
 
     @Override
@@ -33,11 +37,25 @@ public class PhialOfPropulsionItem extends PotionPhialItem {
                     Vec3 offsetPos = new Vec3(dx, dy, dz);
                     BlockPos blockpos = new BlockPos(position.add(offsetPos));
                     BlockState blockState = source.level.getBlockState(blockpos);
+                    // make a new FallingBlockEntity if possible
                     if (!source.level.isEmptyBlock(blockpos)
                             && PistonBaseBlock.isPushable(blockState, source.level, blockpos, Direction.UP, true, Direction.UP)) {
-                        FallingBlockEntity fallingBlockEntity = FallingBlockEntity.fall(source.level, blockpos, source.level.getBlockState(blockpos));
-                        fallingBlockEntity.push((double)dx / (4.0D + source.level.random.nextDouble()), 1.0D, (double)dz / (4.0D + source.level.random.nextDouble()));
+                        FallingBlockEntity fallingBlockEntity = new FallingBlockEntity(EntityType.FALLING_BLOCK, source.level);
+                        fallingBlockEntity.absMoveTo((double)blockpos.getX() + 0.5D, (double)blockpos.getY(), (double)blockpos.getZ() + 0.5D);
+                        // add velocity
+                        double offsetX = (source.level.random.nextDouble() - 0.5D) / 2.0D;
+                        double offsetZ = (source.level.random.nextDouble() - 0.5D) / 2.0D;
+                        fallingBlockEntity.push((double)dx / 4.0D + offsetX, 1.0D, (double)dz / 4.0D + offsetZ);
+                        // change blockstate by reaching a private field
+                        try {
+                            Field field = fallingBlockEntity.getClass().getDeclaredField("blockState");
+                            field.setAccessible(true);
+                            field.set(fallingBlockEntity, blockState);
+                        }
+                        catch(Exception ignored) {}
+                        // finish adding the FallingBlockEntity
                         source.level.destroyBlock(blockpos, false);
+                        source.level.addFreshEntity(fallingBlockEntity);
                     }
                 }
             }
