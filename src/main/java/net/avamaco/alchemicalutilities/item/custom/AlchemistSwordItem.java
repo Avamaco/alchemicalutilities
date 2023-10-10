7902d3b33b73dc656eef0ace3382f9cbea1312cc
Avamaco <Maco_2003@wp.pl>
2023-10-10 11:51:01 +0200

    Cleanup in AlchemistSwordItem

--- src/main/java/net/avamaco/alchemicalutilities/item/custom/AlchemistSwordItem.java
+++ src/main/java/net/avamaco/alchemicalutilities/item/custom/AlchemistSwordItem.java
@@ -22,29 +22,19 @@ public class AlchemistSwordItem extends SwordItem {
 
     @Override
     public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
-        if (pAttacker instanceof Player) {
-            inject(pTarget, (Player)pAttacker);
-            int bonus = getArmorBuff((Player) pAttacker);
-            pAttacker.sendMessage(new TextComponent(Integer.toString(bonus)), pAttacker.getUUID());
+        if (pAttacker instanceof Player player) {
+            int bonus = getArmorBuff(player);
             if (bonus > 0) {
-                Vec3 range = new Vec3(5,5,5);
-                Vec3 center = pTarget.getPosition(1f);
-                AABB aoe = new AABB(center.subtract(range), center.add(range));
-                List<? extends LivingEntity> possibleTargets = pTarget.getLevel().getEntitiesOfClass(LivingEntity.class, aoe);
-                possibleTargets.remove(pTarget);
-                possibleTargets.remove(pAttacker);
-                List<? extends LivingEntity> targets = getClosestEntities(possibleTargets, center, bonus);
-                pAttacker.sendMessage(new TextComponent(targets.toString()), pAttacker.getUUID());
-                for (LivingEntity entity : targets) {
-                    pAttacker.sendMessage(new TextComponent("Working..."), pAttacker.getUUID());
-                    inject(entity, (Player)pAttacker);
-                }
+                List<? extends LivingEntity> targets = getTargets(pTarget, player, bonus);
+                for (LivingEntity entity : targets)
+                    inject(entity, player, false);
             }
+            inject(pTarget, player, true);
         }
         return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
 
-    private void inject(LivingEntity entity, Player player) {
+    private void inject(LivingEntity entity, Player player, boolean consume) {
         if (!(InventoryUtil.checkForPhial(player)))
             return;
         ItemStack phialStack = player.getInventory().getItem(InventoryUtil.getFirstPhial(player));
@@ -52,7 +42,7 @@ public class AlchemistSwordItem extends SwordItem {
         if (phialStack.getItem() instanceof PotionPhialItem) {
             ((PotionPhialItem) phialStack.getItem()).UseOnEntity(entity, player);
 
-            phialStack.shrink(1);
+            phialStack.shrink(consume ? 1 : 0);
             if (phialStack.isEmpty()) {
                 (player).getInventory().removeItem(phialStack);
             }
@@ -68,6 +58,16 @@ public class AlchemistSwordItem extends SwordItem {
                     ret++;
         }
         return ret;
+    }
+
+    private List<? extends LivingEntity> getTargets(LivingEntity target, Player attacker, int cap) {
+        Vec3 range = new Vec3(5,5,5);
+        Vec3 center = target.getPosition(1f);
+        AABB aoe = new AABB(center.subtract(range), center.add(range));
+        List<? extends LivingEntity> possibleTargets = target.getLevel().getEntitiesOfClass(LivingEntity.class, aoe);
+        possibleTargets.remove(target);
+        possibleTargets.remove(attacker);
+            return getClosestEntities(possibleTargets, center, cap);
     }
 
     private List<? extends LivingEntity> getClosestEntities(List<? extends LivingEntity> entities, Vec3 pos, int limit) {
